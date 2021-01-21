ad58600ee33e6cd3fc8a52947680e2d5ed9f5d94
Denisn <denis.nikitin@gotech.io>
2021-01-21 19:09:43 +0200

    fixed path

--- assets/globe/ui-data/ui-data.js
+++ assets/globe/ui-data/ui-data.js
@@ -1,32 +1,32 @@
 const images = [
   { name: "main", src: "//unpkg.com/three-globe/example/img/earth-night.jpg" },
-  { name: "img1", src: "../../assets/globe/assets/img1.jpg" },
-  { name: "img2", src: "../../assets/globe/assets/img2.jpg" },
-  { name: "img3", src: "../../assets/globe/assets/img3.jpg" },
+  { name: "img1", src: "../assets/globe/assets/img1.jpg" },
+  { name: "img2", src: "../assets/globe/assets/img2.jpg" },
+  { name: "img3", src: "../assets/globe/assets/img3.jpg" },
 ];
 
 const socials = [
   {
-    img: "../../assets/globe/assets/twitter.png",
+    img: "../assets/globe/assets/twitter.png",
     url: "",
   },
   {
-    img: "../../assets/globe/assets/linkedin.png",
-    url: "",
-  },
-  ,
-  {
-    img: "../../assets/globe/assets/reddit.png",
+    img: "../assets/globe/assets/linkedin.png",
     url: "",
   },
   ,
   {
-    img: "../../assets/globe/assets/telegram.png",
+    img: "../assets/globe/assets/reddit.png",
     url: "",
   },
   ,
   {
-    img: "../../assets/globe/assets/github.png",
+    img: "../assets/globe/assets/telegram.png",
+    url: "",
+  },
+  ,
+  {
+    img: "../assets/globe/assets/github.png",
     url: "",
   },
 ];
@@ -34,14 +34,14 @@ const socials = [
 const flags = [
   {
     name: "KO",
-    img: "../../assets/globe/assets/korea.png",
+    img: "../assets/globe/assets/korea.png",
   },
   {
     name: "CN",
-    img: "../../assets/globe/assets/china.png",
+    img: "../assets/globe/assets/china.png",
   },
   {
     name: "USA",
-    img: "../../assets/globe/assets/usa.png",
+    img: "../assets/globe/assets/usa.png",
   },
 ];
