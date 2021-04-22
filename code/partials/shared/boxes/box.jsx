c683b249214de48792ddf115fc82f52b519e6cc5
Denisn <denis.nikitin@gotech.io>
2021-04-22 18:08:43 +0300

    a

--- code/partials/shared/boxes/box.jsx
+++ code/partials/shared/boxes/box.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { animations } from "../../../../assets/js/consts/consts";
 import Img from "../Img";
 
-const Box = ({ _body, image }) => {
+const Box = ({ _body, image, _ID, _relativeURL }) => {
+  const generateUrl = (url) => {
+    return `${_relativeURL(url, _ID)}`;
+  };
   return (
     <div className="box" data-aos={animations.fadeTop}>
       {_body}
       <figure>
-        <Img src={image} />
+        <Img src={generateUrl(image)} />
       </figure>
     </div>
   );
--- content/blog/blogs.md
+++ content/blog/blogs.md
@@ -3,6 +3,4 @@ layout: partials/shared/mappers/blog-list-mapper
 list:
   - how-to-choose-an-orbs-guardian/blog.md
   - how-to-choose-an-orbs-guardian2/blog.md
-  - how-to-choose-an-orbs-guardian3/blog.md
-  - how-to-choose-an-orbs-guardian4/blog.md
 ---
