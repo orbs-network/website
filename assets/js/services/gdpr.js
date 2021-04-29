7ccd9c79310b036a9c0499d38123d0b02e78dc1f
ci-build <ci-build@orbs.com>
2021-04-29 15:47:26 +0000

    [skip ci] Deploy

---
+++ assets/js/services/gdpr.js
@@ -0,0 +1,10 @@
+const init = () => {
+  glowCookies.start("en", {
+    style: 1,
+    analytics: "G-FH87DE17XF",
+    facebookPixel: "990955817632355",
+    policyLink: "https://link-to-your-policy.com",
+  });
+};
+
+export default { init };
