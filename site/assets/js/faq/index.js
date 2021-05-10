65fabd5c80e72f7c4f659731bb839c673f8b0223
Denisn <denis.nikitin@gotech.io>
2021-05-10 12:56:19 +0300

    updated ci

---
+++ site/assets/js/faq/index.js
@@ -0,0 +1,34 @@
+import { addEvent, getElement, getElements } from "../heplers.js";
+import { setOpeningBoxEvents } from "../components/opening-box.js";
+import { init } from "../index.js";
+
+window.onload = () => {
+  init();
+  setOpeningBoxEvents();
+  addEventsToMobile();
+};
+
+const addEventsToMobile = () => {
+  const selectedConteiner = getElement(".faq-mobile-menu-selected");
+  if (!selectedConteiner) return;
+  addEvent(selectedConteiner, "click", toggleMenu);
+  const mobileLinks = getElements(".faq-mobile-menu-link");
+  mobileLinks.forEach((link) => {
+    addEvent(link, "click", () => handleLinkClicked(link));
+  });
+};
+
+const handleLinkClicked = (link) => {
+  const selectedElement = getElement(".faq-mobile-menu-selected");
+  selectedElement.textContent = link.textContent;
+  toggleMenu();
+};
+
+const toggleMenu = () => {
+  const menu = getElement(".faq-mobile-menu-list");
+  const isActive = menu.classList.contains("faq-mobile-menu-list-active");
+  if (!isActive) {
+    return menu.classList.add("faq-mobile-menu-list-active");
+  }
+  menu.classList.remove("faq-mobile-menu-list-active");
+};
