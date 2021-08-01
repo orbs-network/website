e9aaca42bcea7b5dab673af6a3071b2707dcce2b
Denisn <denis.nikitin@gotech.io>
2021-08-01 17:44:45 +0300

    a

--- code/pages/page.js
+++ code/pages/page.js
@@ -30,6 +30,8 @@ const Page = ({
           src="https://platform.twitter.com/widgets.js"
           charSet="utf-8"
         ></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <link
           href="https://unpkg.com/aos@2.3.1/dist/aos.css"
           rel="stylesheet"
