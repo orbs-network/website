c4e12e98bdc661a9176af0b8aa6e32d557f8aa70
Denisn <denis.nikitin@gotech.io>
2021-08-16 10:58:29 +0300

    a

--- code/partials/home/cards/container/index.js
+++ code/partials/home/cards/container/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CardsContainer = ({ list }) => {
-  return <div>{list}</div>;
+  return <div style={{ display: "none" }}>{list}</div>;
 };
 
 export default CardsContainer;
