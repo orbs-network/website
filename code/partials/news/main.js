ae604e44deefa79cef055ea867c3e95833bfc71d
Denisn <denis.nikitin@gotech.io>
2022-02-07 10:54:34 +0200

    a

--- code/partials/news/main.js
+++ code/partials/news/main.js
@@ -24,7 +24,7 @@ const Main = ({ mainPost, postsTop, videoSection, loadMoreButton, subscribe, _re
                 <Posts id='news-posts-bottom' />
             </div>
             {loadMoreButton}
-
+            <script type='module' src={generateUrl(`/assets/js/news/index.js`)} />
         </main>
     )
 }
