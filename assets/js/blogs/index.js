06e491c70d700c58d72e9171c08a2bde97d004a2
ci-build <ci-build@orbs.com>
2022-03-13 13:02:32 +0000

    [ci skip] Deploy

--- assets/js/blogs/index.js
+++ assets/js/blogs/index.js
@@ -1,12 +1,25 @@
 import { getElement, getElementAttribute, getElements } from "../heplers.js";
 import { init } from "../index.js";
+import analytics from "../services/analytics.js";
 import { initInfiniteScrolling } from "./infinite-sctoll.js";
 
 let blogsList = [];
 let blogsContainer;
+const list = getElements(".blog-list .blog-list-blog");
+
+
+
+const addAnalyticsEvents = () => {
+    list.forEach(elem => {
+      elem.addEventListener('click', () => {
+        const title = elem.getAttribute('data-title')
+        analytics.sendEvent(analytics.events.blogClicked, {name: title} )
+      })
+    })
+}
 
 const handleBlogListOnLoad = () => {
-  const list = getElements(".blog-list .blog-list-blog");
+
   blogsList = list;
   blogsContainer = getElement(".blog-list");
   blogsContainer.innerHTML = "";
@@ -30,4 +43,5 @@ const handleBlogImage = (blog) => {
 window.onload = () => {
   init();
   handleBlogListOnLoad();
+  addAnalyticsEvents()
 };
