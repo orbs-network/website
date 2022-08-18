5037494062bf0a2d3eec59781068a93d8699bfdf
ci-build <ci-build@orbs.com>
2022-08-18 13:16:42 +0000

    [ci skip] Deploy

--- assets/js/keys.js
+++
@@ -1,6 +0,0 @@
-export const USER_POST_USER_ID = "user_koNkM5rLanEFftLBUpCe1";
-export const USER_POST_SERVICE_ID = "service_38tdn3b";
-export const USER_POST_CONTACT_TEMPLATE_ID = "template_fnk4k6f";
-export const USER_POST_SUBSCRIVE_TEMPLATE_ID = "template_queirw7";
-export const GDPR_SEEN = "gdpr_seen";
-export const GDPR_ACCEPTED = "gdpr_accepted";
