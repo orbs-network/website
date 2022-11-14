5b5011ded5b06077c6af1ac95c5e9095d8cdaf35
antongotech <105588893+antongotech@users.noreply.github.com>
2022-11-14 10:20:48 +0200

    brandAssets page added

---
+++ code/partials/brandAssets/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <main style={{width: '100%', height: '100px', background: 'red'}}>
+
+        </main>
+    );
+};
+
+export default Main;
